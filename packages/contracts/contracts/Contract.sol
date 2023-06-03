beec95cf4901e853d77b0e45d85ae0f434a817ff
Kenk <kenghin_lim@hotmail.com>
2023-06-03 15:02:09 +0800

    feat: add 721 contract

--- packages/contracts/contracts/Contract.sol
+++ packages/contracts/contracts/Contract.sol
@@ -1,16 +1,99 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.12;
 
-contract Contract {
-    /**
-     * @dev Deposit ETH to the contract
-     */
-    function deposit() external payable {}
+// import erc721
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import { ByteHasher } from "./helpers/ByteHasher.sol";
+import { IWorldID } from "./interfaces/IWorldID.sol";
+
+contract Contract is ERC721 {
+    using ByteHasher for bytes;
+
+    /// @notice Thrown when attempting to reuse a nullifier
+    error InvalidNullifier();
+
+    /// @dev The World ID instance that will be used for verifying proofs
+    IWorldID internal immutable worldId;
+
+    /// @dev The contract's external nullifier hash
+    uint256 internal immutable externalNullifier;
+
+    /// @dev The World ID group ID (always 1)
+    uint256 internal immutable groupId = 1;
+
+    /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
+    mapping(uint256 => bool) internal nullifierHashes;
+
+    uint256 currentId = 1;
+
+    mapping(uint256 => string) internal tokenIdToUri;
+
+    /// @param _worldId The WorldID instance that will verify the proofs
+    /// @param _appId The World ID app ID
+    /// @param _actionId The World ID action ID
+    constructor(IWorldID _worldId, string memory _appId, string memory _actionId) ERC721("Contract", "CONTRACT") {
+        worldId = _worldId;
+        externalNullifier = abi.encodePacked(abi.encodePacked(_appId).hashToField(), _actionId).hashToField();
+    }
+
+    /// @param signal An arbitrary input from the user, usually the user's wallet address (check README for further details)
+    /// @param root The root of the Merkle tree (returned by the JS widget).
+    /// @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget).
+    /// @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the JS widget).
+    /// @param ipfsHash The IPFS hash of the content to be minted
+    /// @dev Feel free to rename this method however you want! We've used `claim`, `verify` or `execute` in the past.
+    function verifyAndExecute(
+        address signal,
+        uint256 root,
+        uint256 nullifierHash,
+        uint256[8] calldata proof,
+        string memory ipfsHash
+    ) public {
+        // First, we make sure this person hasn't done this before
+        if (nullifierHashes[nullifierHash]) revert InvalidNullifier();
+
+        // We now verify the provided proof is valid and the user is verified by World ID
+        worldId.verifyProof(
+            root,
+            groupId,
+            abi.encodePacked(signal).hashToField(),
+            nullifierHash,
+            externalNullifier,
+            proof
+        );
+
+        // We now record the user has done this, so they can't do it again (proof of uniqueness)
+        nullifierHashes[nullifierHash] = true;
+
+        // Finally, execute your logic here, for example issue a token, NFT, etc...
+        // Make sure to emit some kind of event afterwards!
+        mint();
+        tokenIdToUri[currentId] = ipfsHash;
+    }
+
+    function mint() private {
+        _mint(msg.sender, currentId);
+        currentId++;
+    }
+
+    function burn(uint256 tokenId) public {
+        _burn(tokenId);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return currentId;
+    }
 
     /**
-     * @dev Withdraw ETH from the contract
+     * @dev Returns the base URI for the token.
+     * @param tokenId The token to get the base URI of
+     * @return The IPFS hash of the token
      */
-    function withdraw() external {
-        payable(msg.sender).transfer(address(this).balance);
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireMinted(tokenId);
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked("ipfs://", tokenIdToUri[tokenId])) : "";
     }
 }
